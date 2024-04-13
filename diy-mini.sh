926a221043499e77d415ec166d6a2019e1d9dd82
lw23305 <76029562+lw23305@users.noreply.github.com>
2024-04-13 22:05:50 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -16,7 +16,7 @@ sed -i "s/KERNEL_TESTING_PATCHVER:=*.*/KERNEL_TESTING_PATCHVER:=5.15/g" target/l
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # TTYD 免登录
-# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
