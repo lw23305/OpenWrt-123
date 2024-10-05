0264e9fea6a02a2e6d86bcd1a6c5b41b73195335
lw23305 <76029562+lw23305@users.noreply.github.com>
2024-10-05 22:46:45 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -30,9 +30,6 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# 添加额外插件
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-
 # 科学上网插件
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/deng/luci-app-passwall
 # git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/deng/passwall-packages
