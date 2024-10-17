17d70f23783860253442cc86a7410b61d631fb7c
lw23305 <76029562+lw23305@users.noreply.github.com>
2024-10-17 09:21:12 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -41,10 +41,6 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
-# mosdns
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 # find ./ | grep Makefile | grep mosdns | xargs rm -f
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
